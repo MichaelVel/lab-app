6b07bf6efb2541609afa80abdefe0c705455e19f
Michael Vel <msvelandiag@unal.edu.co>
2022-11-09 18:50:36 -0500

    main structure of creation form

--- client/src/routes/create-challenge.tsx
+++ client/src/routes/create-challenge.tsx
@@ -1,7 +1,8 @@
 import MainLayout from "../main/main";
-import {Grid, Box, Button, Divider, ButtonGroup} from "@mui/material";
+import {Grid, Box, Stack, Button, Divider, ButtonGroup, TextField} from "@mui/material";
 
 import { AllowAnswers , VisibilityOption} from '../main/creation-config';
+import Section from "../main/section";
 
 export default function CreateChallenge() {
     return (
@@ -38,7 +39,57 @@ export default function CreateChallenge() {
                 </Box>
             }
             main={
-                <div></div>
+                <div>
+                    <Grid
+                        container
+                        justifyContent="center"
+                        spacing={1}
+                    >
+                        <Grid item xs={10}>
+                            <Section
+                                name="¿Cual es el nombre de tu reto?"
+                                elements={
+                                    <TextField size="small" label="Titulo"/>
+                                }
+                            />
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Section
+                                name="Contexto"
+                                elements={
+                                    <Stack direction="row">
+                                        <h3>Tema Principal</h3> 
+                                        <TextField size="small" label="Titulo"/>
+                                    </Stack>
+                                }
+                            />
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Section
+                                name="¡Manos a la obra!"
+                                elements={
+                                    <TextField size="small" label="Titulo"/>
+                                }
+                            />
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Section
+                                name="Para el alumno"
+                                elements={
+                                    <TextField size="small" label="Titulo"/>
+                                }
+                            />
+                        </Grid>
+                        <Grid item xs={10}>
+                            <Section
+                                name="Explicación"
+                                elements={
+                                    <TextField size="small" label="Titulo"/>
+                                }
+                            />
+                        </Grid>
+                    </Grid>
+                </div>
             }
         />
       </form>

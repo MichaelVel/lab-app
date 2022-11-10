0c8db2a0169e7e2908f4f402a36f01f3547bb8af
Michael Vel <msvelandiag@unal.edu.co>
2022-11-10 17:49:33 -0500

    first draft of creation view

--- client/src/routes/create-challenge.tsx
+++ client/src/routes/create-challenge.tsx
@@ -3,10 +3,13 @@ import {Grid, Box, Stack, Button, Divider, ButtonGroup, TextField} from "@mui/ma
 
 import { AllowAnswers , VisibilityOption} from '../main/creation-config';
 import Section from "../main/section";
+import InputChipsArray from "../main/inputs/chip-array";
+import ListInput from "../main/inputs/input-list";
+import {Form} from "react-router-dom";
 
 export default function CreateChallenge() {
     return (
-      <form>
+      <Form method="get">
         <MainLayout 
             sideBar={
                 <Box sx={{
@@ -39,59 +42,124 @@ export default function CreateChallenge() {
                 </Box>
             }
             main={
-                <div>
-                    <Grid
-                        container
-                        justifyContent="center"
-                        spacing={1}
-                    >
-                        <Grid item xs={10}>
-                            <Section
-                                name="¿Cual es el nombre de tu reto?"
-                                elements={
-                                    <TextField size="small" label="Titulo"/>
-                                }
-                            />
-                        </Grid>
-                        <Grid item xs={10}>
-                            <Section
-                                name="Contexto"
-                                elements={
-                                    <Stack direction="row">
-                                        <h3>Tema Principal</h3> 
-                                        <TextField size="small" label="Titulo"/>
-                                    </Stack>
-                                }
-                            />
-                        </Grid>
-                        <Grid item xs={10}>
-                            <Section
-                                name="¡Manos a la obra!"
-                                elements={
-                                    <TextField size="small" label="Titulo"/>
-                                }
-                            />
-                        </Grid>
-                        <Grid item xs={10}>
-                            <Section
-                                name="Para el alumno"
-                                elements={
-                                    <TextField size="small" label="Titulo"/>
-                                }
-                            />
-                        </Grid>
-                        <Grid item xs={10}>
-                            <Section
-                                name="Explicación"
-                                elements={
-                                    <TextField size="small" label="Titulo"/>
-                                }
-                            />
-                        </Grid>
+                <Grid
+                    container
+                    justifyContent="center"
+                    spacing={1}
+                >
+                    <Grid item xs={10}>
+                        <Section
+                            name="¿Cual es el nombre de tu reto?"
+                            elements={
+                                <TextField size="small" label="Titulo" name="title"/>
+                            }
+                        />
                     </Grid>
-                </div>
+                    <Grid item xs={10}>
+                        <Section
+                            name="Contexto"
+                            elements={
+                                <Grid container spacing={1}>
+                                    <Grid item xs={6} sx={{padding:'0.8em'}}>
+                                        <h3>Tema Principal</h3> 
+                                        <TextField 
+                                            size="small" 
+                                            name="mainTopic"
+                                            label="Titulo"
+                                        />
+                                        <h3>Otros Temas</h3> 
+                                        <InputChipsArray />
+                                    </Grid>
+                                    <Grid item xs={6} sx={{padding:'0.8em'}}>
+                                        <h3>Descripción</h3> 
+                                        <TextField 
+                                            size="small"
+                                            label="Descripción"
+                                            multiline
+                                            minRows={6}
+                                            fullWidth
+                                            name="description"
+                                        />
+                                    </Grid>
+                                </Grid>
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Section
+                            name="¡Manos a la obra!"
+                            elements={
+                                <Grid container spacing={1}>
+                                    <Grid item xs={6} sx={{padding:'0.8em'}}>
+                                        <h3>Materiales</h3> 
+                                        <ListInput 
+                                            listName="Materiales añadidos"
+                                            inputName='materials'/>
+                                    </Grid>
+                                    <Grid item xs={6} sx={{padding:'0.8em'}}>
+                                        <h3>Instrucciones</h3> 
+                                        <ListInput 
+                                            listName="Pasos del experimento"
+                                            inputName='instructions'/>
+                                    </Grid>
+                                </Grid>
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Section
+                            name="Para el alumno"
+                            elements={
+                                <Grid container spacing={1}>
+                                    <Grid item xs={12} sx={{padding:'0.8em'}}>
+                                        <h3>Instrucciones de entrega</h3> 
+                                        <TextField 
+                                            size="small"
+                                            label="Descripción"
+                                            multiline
+                                            minRows={4}
+                                            fullWidth
+                                            name="submitInstructions"
+                                        />
+                                    </Grid>
+                                </Grid>
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Section
+                            name="Explicación"
+                            elements={
+                                <Grid container spacing={1}>
+                                    <Grid item xs={6} sx={{padding:'0.8em'}}>
+                                        <p>
+                                        En esta sección el maestro debe subir
+                                        la explicación del experimento: el que
+                                        el como y un desarrollo breve del tema
+                                        principal. Esta explicación puede estar 
+                                        en un formato PDF, JPEG o el maestro puede
+                                        optar por subir un video a youtube y subir
+                                        el link. TODO: El desarrollo de esta sección
+                                        se llevara a cabo en su totalidad en el 
+                                        proximo sprint.
+                                        </p> 
+                                    </Grid>
+                                    <Grid item xs={6} sx={{padding:'0.8em'}}>
+                                        <TextField 
+                                            size="small"
+                                            label="Descripción"
+                                            multiline
+                                            minRows={4}
+                                            fullWidth
+                                        />
+                                    </Grid>
+                                </Grid>
+                            }
+                        />
+                    </Grid>
+                </Grid>
             }
         />
-      </form>
+      </Form>
     );
 }

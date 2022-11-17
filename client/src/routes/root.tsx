fc69cbd892deffb0fcc5b10f92ca18676db2dc44
Michael Vel <msvelandiag@unal.edu.co>
2022-11-17 11:32:17 -0500

    config/test proxy between projects

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,15 +1,31 @@
-import { ReactElement } from "react";
-
+import { ReactElement, useState, useEffect } from "react";
 import { Outlet } from "react-router";
-
 import Header from '../header/Header';
 
+const callBackendApi =  async () => {
+  const response = await fetch('/express');
+  console.log(response);
+  const body = await response.json();
+  
+  if (response.status !== 200) { throw Error(body.message) }
+  return body;
+}
+
 export default function Root(): ReactElement {
+    const [data, setData] = useState("");
+
+    useEffect( () => {
+      callBackendApi()
+        .then(res => setData(res.express))
+        .catch(err => console.log(err))
+    });
+
     return (
     <div>
         <Header/>
         <main>
             <Outlet/> 
+            <p>{data}</p>
         </main>
     </div>
     );
